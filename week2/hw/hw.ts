d92d88192d897cbf036244e2d5186817628770ee
benredmond <bjr0206@gmail.com>
2019-04-01 19:28:54 -0500

    working on hw2

--- week2/hw/hw.ts
+++ week2/hw/hw.ts
@@ -1,4 +1,6 @@
 import request, { Response } from 'request'; // I hate myself everytime I type this
+import fetch from 'node-fetch';
+import { lstat } from 'fs';
 require('dotenv').config();
 interface WeatherUpdate {
 	location: string; // i.e. "Vanderbilt University"
@@ -8,38 +10,145 @@ interface WeatherUpdate {
 	name: string; // your name - use this from process.env
 }
 
+interface OpenStreetMap {
+	lat: string;
+	lon: string;
+}
+interface DarkSkyCurrently {
+	summary: string;
+	temperature: number;
+}
+
+interface DarkSky {
+	currently: DarkSkyCurrently;
+}
+
+interface SlackResponse {
+	status: number;
+	sha1: string;
+}
+
 const callbacks = (
 	location: string,
 	slackUsername: string,
-	callback: (body: any) => void
+	callback: (body: SlackResponse) => void
 ): void => {
+	let weatherInfo: WeatherUpdate = {
+		location: location,
+		weather: '',
+		lat: 0,
+		lon: 0,
+		name: '',
+	};
+
+	if (process.env.NAME) weatherInfo.name = process.env.NAME;
+	const latLongUrl = `https://nominatim.openstreetmap.org/?format=json&q=${location}&format=json&limit=3&email=${process.env.EMAIL}`;
 	request(
 		// this is just the first call to request. You'll need multiple
-		'YOUR_URL',
-		(error: Error, response: Response, body: object): void => {
+		latLongUrl,
+		(error: Error, response: Response, geocodeBody: string): void => {
 			if (error) {
 				console.log(error);
 			} else {
-				console.log(body);
+				let latlon: OpenStreetMap[] = JSON.parse(geocodeBody);
+				weatherInfo.lat = parseFloat(latlon[0].lat);
+				weatherInfo.lon = parseFloat(latlon[0].lon);
+				const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_TOKEN}/${weatherInfo.lat},${weatherInfo.lon}`;
+
+				request(
+					darkSkyUrl,
+					(error: Error, response: Response, darkSkyBody: string): void => {
+						if (error) {
+							console.log(error);
+						} else {
+							let weatherData: DarkSky = JSON.parse(darkSkyBody);
+							weatherInfo.weather = `It's ${weatherData.currently.summary} and it's ${weatherData.currently.temperature} degrees.`
+							const slackUrl = `https://send-to-slack-nfp4cc31q.now.sh/?user=C9S0DF3BR&data=${JSON.stringify(weatherInfo)}`;
+
+							request(
+								slackUrl,
+								(error: Error, response: Response, slackBody: string): void => {
+									if (error) {
+										console.log(error);
+									} else {
+										callback(JSON.parse(slackBody));
+									}
+								}
+							)
+						}
+					}
+				)
 			}
 		}
 	);
 };
 
 // change Promise<object> to Promise<TheTypeThatYouAreMaking> for both functions
-const promises = (location: string, slackUsername: string): Promise<object> => {
-	// use fetch
-};
-export const asyncAwait = async (location: string, slackUsername: string): Promise<object> => {
-	// use fetch
+const promises = (location: string, slackUsername: string): Promise<SlackResponse> => {
+	let weatherInfo: WeatherUpdate = {
+		location: location,
+		weather: '',
+		lat: 0,
+		lon: 0,
+		name: '',
+	};
+	if (process.env.NAME) weatherInfo.name = process.env.NAME;
+	const latLonUrl = `https://nominatim.openstreetmap.org/?format=json&q=${weatherInfo.location}&format=json&limit=3&email=${process.env.EMAIL}`;
+
+	let result = fetch(latLonUrl)
+		.then(data => {
+			return data.json();
+		}).then((latlon: OpenStreetMap[]) => {
+			weatherInfo.lat = parseFloat(latlon[0].lat);
+			weatherInfo.lon = parseFloat(latlon[0].lon);
+			const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_TOKEN}/${weatherInfo.lat},${weatherInfo.lon}`;
+			return fetch(darkSkyUrl);
+		}).then(data => {
+			return data.json();
+		}).then((weatherData: DarkSky) => {
+			weatherInfo.weather = `It's ${weatherData.currently.summary} and it's ${weatherData.currently.temperature} degrees.`
+			const slackUrl = `https://send-to-slack-nfp4cc31q.now.sh/?user=C9S0DF3BR&data=${JSON.stringify(weatherInfo)}`;
+			return fetch(slackUrl);
+		}).then(data => {
+			return data.json();
+		})
+		.catch(err => console.log(err))
+
+	return result;
+}
+
+export const asyncAwait = async (location: string, slackUsername: string): Promise<SlackResponse> => {
+	let weatherInfo: WeatherUpdate = {
+		location: location,// i.e. "Vanderbilt University"
+		weather: '', // the format specified in the README
+		lat: 0,
+		lon: 0,
+		name: '',
+	};
+
+	if (process.env.NAME) weatherInfo.name = process.env.NAME;
+	const latLonUrl = `https://nominatim.openstreetmap.org/?format=json&q=${weatherInfo.location}&format=json&limit=3&email=${process.env.EMAIL}`;
+	let latlon: OpenStreetMap[];
+	latlon = await (await fetch(latLonUrl)).json();
+
+	weatherInfo.lat = parseFloat(latlon[0].lat);
+	weatherInfo.lon = parseFloat(latlon[0].lon);
+
+	const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_TOKEN}/${weatherInfo.lat},${weatherInfo.lon}`;
+	let currWeather: DarkSky;
+	currWeather = await (await fetch(darkSkyUrl)).json();
+	weatherInfo.weather = `It's ${currWeather.currently.summary} and it's ${currWeather.currently.temperature} degrees.`
+
+	const slackUrl = `https://send-to-slack-nfp4cc31q.now.sh/?user=C9S0DF3BR&data=${JSON.stringify(weatherInfo)}`;
+	return (await fetch(slackUrl)).json();
 };
 
 // all the console.logs should log what the send-to-slack API returns
-callbacks('Vanderbilt University', 'YOUR_SLACK_USER_ID', body => {
-	console.log(body);
-}); // feel free to change the place. It'll be more interesting if everyone's not doing the same place.
-promises('Vanderbilt University', 'D44FTVCHJ').then(data => console.log(data));
+// callbacks('Vanderbilt University', 'YOUR_SLACK_USER_ID', body => {
+// 	console.log(body);
+// }); // feel free to change the place. It'll be more interesting if everyone's not doing the same place.
+// promises('Vanderbilt University', 'D44FTVCHJ').then(data => console.log(data));
 
-(async () => {
-	console.log(await asyncAwait('Vanderbilt University', 'D44FTVCHJ'));
-})();
+// (async () => {
+// 	console.log(await asyncAwait('Vanderbilt University', 'D44FTVCHJ'));
+// })();
--- week2/hw/server.ts
+++ week2/hw/server.ts
@@ -1,9 +1,10 @@
 import Koa from 'koa';
+import { asyncAwait } from './hw';
 
 const app = new Koa();
 
 app.use(async ctx => {
-	ctx.body = 'Hello world'; // replace this with the call to async await with your desired location .
+	ctx.body = await asyncAwait('Vanderbilt University', 'D44FTVCHJ'); // replace this with the call to async await with your desired location .
 });
 
 app.listen(3000);
